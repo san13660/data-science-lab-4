bf638e635c5d9369461a1139cb23eb9b0858205d
Fernanda Estrada <est14198@uvg.edu.gt>
2020-08-27 02:10:29 -0600

    correcciones

--- graficar_hist.py
+++ graficar_hist.py
@@ -1,7 +1,20 @@
+# Universidad del Valle de Guatemala
+# Data Science 1 - Seccion 10
+# Christopher Sandoval 13660
+# Maria Fernanda Estrada 14198
+# *
+# *
+# 20/08/2020
+# Programa para graficar el top 10 de palabras mas frecuentes
+
+
+
+# Librerias
 import matplotlib.pyplot as plt
 import pandas as pd
 import sys
 
+# Leer y mostrar las palabras mas frecuentes en un histograma
 df = pd.read_csv(sys.argv[1])
 df.plot.bar(x='Word',y='Count')
 plt.show()
