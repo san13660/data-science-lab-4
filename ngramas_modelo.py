c915a51c1cdddb3fda4ae72409f4790cb1bcfb62
Fernanda Estrada <est14198@uvg.edu.gt>
2020-08-27 02:15:34 -0600

    correcciones

--- ngramas_modelo.py
+++ ngramas_modelo.py
@@ -1,23 +1,33 @@
-import nltk
+# Universidad del Valle de Guatemala
+# Data Science 1 - Seccion 10
+# Christopher Sandoval 13660
+# Maria Fernanda Estrada 14198
+# *
+# *
+# 20/08/2020
+# Programa para generar el modelo con n-gramas
 
+
+# Librerias
+import nltk
 from nltk.util import ngrams
 from nltk.lm.preprocessing import padded_everygram_pipeline
 from nltk import word_tokenize, sent_tokenize
 from nltk.lm import MLE
 import pickle
-
 import sys
 
+# Leer archivo ya preprocesado
 print('Cargando archivo...')
-
 file = open(sys.argv[1], encoding="utf8")
 text = file.read()
-
 text = text[0:10000000]
 
+# Tokenizar para generar n-gramas
 tokenized_text = [list(map(str.lower, word_tokenize(sent))) 
               for sent in sent_tokenize(text)]
 
+# Crear n-gramas y modelo. Se calculan las probabilidaddes de cada n-grama
 print('Creando Modelo...')
 
 n = 3
